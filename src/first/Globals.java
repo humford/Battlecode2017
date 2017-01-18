adf2c0bb1e866b0d86be1129f5369ffa4a89bf2b
kohtaroyamakawa <kohyamakawa@gmail.com>
2017-01-18 09:30:00 -0500

    Update Globals.java

--- src/first/Globals.java
+++ src/first/Globals.java
@@ -18,7 +18,7 @@ public class Globals {
     // Keep important numbers here
     static int GARDENER_MAX = 6;
     static int LUMBERJACK_MAX = 5;
-
+static int ROUND_CHANGE = 500;
     
     static boolean chargeActivate = true;
     static boolean archonDetected = false;
