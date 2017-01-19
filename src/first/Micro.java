7e5ed8f83356633fb853e278d4ddcddb072e11bc
kohtaroyamakawa <kohyamakawa@gmail.com>
2017-01-18 20:44:03 -0500

    Update Micro.java

--- src/first/Micro.java
+++ src/first/Micro.java
@@ -1,10 +1,18 @@
 package first;
 import battlecode.common.*;
 public class Micro{
+  static final boolean CONDENSED = false;
   //First scout always faces outward, makes sure the penta shot does not hit the archons
   public static void firstScoutShots(){}
   
-  //
+  //Only attack while their obstacle is still there.  When enemy only needs one shot, flag it
+  public static void finishHim(){}
+  
+  //If three robots sense each other then stop firing pentashots
+  public static boolean becomeCondensed(){}
+  
+  
+  
   
   
   
