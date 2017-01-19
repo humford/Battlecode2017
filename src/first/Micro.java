4e7b429696627d84fef1a2d13b93cbadca433384
kohtaroyamakawa <kohyamakawa@gmail.com>
2017-01-18 20:17:04 -0500

    Update Micro.java

--- src/first/Micro.java
+++ src/first/Micro.java
@@ -1,1 +1,11 @@
-
+package first;
+import battlecode.common.*;
+public class Micro{
+  //First scout always faces outward, makes sure the penta shot does not hit the archons
+  public static void firstScoutShots(){}
+  
+  //
+  
+  
+  
+}
