acc8a8e3ecf40992c32f29bd65c5c6282934ff46
rabbott99 <rabbott4927@gmail.com>
2017-01-21 02:47:20 -0500

    Added better_dodge, which has a better (untested) dodge function which attempts to take in every bullet sensed and react accordingly

---
+++ src/better_dodge/BotArchon.java
@@ -0,0 +1,139 @@
+package first;
+import battlecode.common.*;
+
+class BotArchon extends Globals {
+	private static MapLocation birthLoc;
+	public static boolean underAttack = false;
+	
+	public static void loop() throws GameActionException {
+		
+		birthLoc = rc.getLocation();
+		
+		if(rc.getLocation() == rc.getInitialArchonLocations(myTeam)[0])
+		{
+			rc.broadcast(BuildQueue.POINTER_CHANNEL, BuildQueue.low_endpoint);
+			
+			Messaging.broadcastLocation(initialArchonLocations[0], STRIKE_LOC_CHANNEL);
+			
+			if(rc.senseNearbyTrees().length > 3)
+			{
+				BuildQueue.enqueue(RobotType.GARDENER);
+				BuildQueue.enqueue(RobotType.SCOUT);
+				BuildQueue.enqueue(RobotType.LUMBERJACK);
+				BuildQueue.enqueue(RobotType.GARDENER);
+				BuildQueue.enqueue(RobotType.SCOUT);
+				BuildQueue.enqueue(RobotType.LUMBERJACK);
+			
+				for(int i = 0; i < 5; i ++)
+				{
+					BuildQueue.enqueue(RobotType.GARDENER);
+					BuildQueue.enqueue(RobotType.LUMBERJACK);
+					BuildQueue.enqueue(RobotType.GARDENER);
+					BuildQueue.enqueue(RobotType.SOLDIER);
+				}
+			}
+			
+			else
+			{
+				BuildQueue.enqueue(RobotType.GARDENER);
+				BuildQueue.enqueue(RobotType.SCOUT);
+				BuildQueue.enqueue(RobotType.GARDENER);
+				BuildQueue.enqueue(RobotType.SCOUT);
+				BuildQueue.enqueue(RobotType.SCOUT);
+				BuildQueue.enqueue(RobotType.GARDENER);
+				BuildQueue.enqueue(RobotType.SCOUT);
+				BuildQueue.enqueue(RobotType.LUMBERJACK);
+		
+				for(int i = 0; i < 5; i ++)
+				{
+					BuildQueue.enqueue(RobotType.GARDENER);
+					BuildQueue.enqueue(RobotType.LUMBERJACK);
+					BuildQueue.enqueue(RobotType.GARDENER);
+					BuildQueue.enqueue(RobotType.SOLDIER);
+				}
+			}
+			
+			BuildQueue.printQueue();
+		}
+		
+        while (true) {
+            try {
+            	
+            	loop_common();
+            	rc.broadcast(GARDENER_COUNT_CHANNEL, rc.readBroadcast(GARDENER_SUM_CHANNEL));
+            	rc.broadcast(GARDENER_SUM_CHANNEL, 0);
+
+            	if(BuildQueue.getLength() <= 0)
+            	{
+            		if(rc.readBroadcast(GARDENER_COUNT_CHANNEL) < GARDENER_LOWER_LIMIT)
+                	{
+            			BuildQueue.enqueue(RobotType.GARDENER);
+                	}
+            		else
+            		{
+            			BuildQueue.enqueue(RobotType.GARDENER);
+    					BuildQueue.enqueue(RobotType.LUMBERJACK);
+    					BuildQueue.enqueue(RobotType.SOLDIER);
+    					BuildQueue.enqueue(RobotType.SOLDIER);
+            		}
+            	}
+            	
+            	if(rc.getTeamBullets() > VICTORY_CASH)
+            	{
+            		int x = (int)rc.getTeamBullets() - VICTORY_CASH;
+            		rc.donate(x - x%10);
+            	}
+            	
+            	
+            	//Pathfinding.wander();
+            	
+            	Micro.dodge();
+            	
+            	RobotInfo[] enemyBots = rc.senseNearbyRobots(-1, them);
+            	
+            	if(enemyBots.length > 2)
+            	{
+            		Messaging.broadcastLocation(rc.getLocation(), DEFENSE_LOC_CHANNEL);
+            		rc.broadcast(DEFENSE_CHANNEL, 1);
+            		underAttack = true;
+            	}
+            	
+            	else if(underAttack)
+            	{
+            		rc.broadcast(DEFENSE_CHANNEL, 0);
+            		underAttack = false;
+            	}
+                
+            	Direction dir = randomDirection();
+            	
+            	if(rc.readBroadcast(GARDENER_COUNT_CHANNEL) == 0)
+            	{
+            		if(rc.canBuildRobot(RobotType.GARDENER, dir))
+                	{
+            			rc.canBuildRobot(RobotType.GARDENER, dir);
+                	}
+            	}
+            	
+                if(BuildQueue.getLength() > 0)
+                {
+                	if(rc.canBuildRobot(BuildQueue.peak(), dir))
+                	{
+                		rc.buildRobot(BuildQueue.dequeue(), dir);
+                	}
+                }
+                
+                if(!rc.hasMoved())
+                {
+                	if(rc.getLocation().distanceTo(birthLoc) > 5)
+                	{
+                		Pathfinding.moveTo(birthLoc);
+                	}
+                }
+                	
+                Clock.yield();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+	}
+}
---
+++ src/better_dodge/Pathfinding.java
@@ -0,0 +1,122 @@
+package first;
+import battlecode.common.*;
+
+public class Pathfinding extends Globals {
+	/**
+     * Attempts to move in a given direction, while avoiding small obstacles directly in the path.
+     *
+     * @param dir The intended direction of movement
+     * @return true if a move was performed
+     * @throws GameActionException
+     */
+    public static boolean tryMove(Direction dir) throws GameActionException {
+        return tryMove(dir,20,3);
+    }
+
+
+    /**
+     * Attempts to move in a given direction, while avoiding small obstacles direction in the path.
+     *
+     * @param dir The intended direction of movement
+     * @param degreeOffset Spacing between checked directions (degrees)
+     * @param checksPerSide Number of extra directions checked on each side, if intended direction was unavailable
+     * @return true if a move was performed
+     * @throws GameActionException
+     */
+    
+    
+    
+    private static boolean tryMove(Direction dir, float degreeOffset, int checksPerSide) throws GameActionException {
+
+        // First, try intended direction
+    	BulletInfo bullets[] = rc.senseNearbyBullets();
+    	
+    	boolean isSafe = true;
+    	MapLocation loc = rc.getLocation().add(dir);
+    	
+    	for(BulletInfo b : bullets)
+    	{
+    		if(willCollideWith(b, loc))
+    		{
+    			isSafe = false;
+    			break;
+    		}
+    	}
+    	
+        if (!rc.hasMoved() && rc.canMove(dir) && isSafe) {
+        	
+            rc.move(dir);
+            return true;
+        }
+
+        // Now try a bunch of similar angles
+        //boolean moved = rc.hasMoved();
+        int currentCheck = 1;
+        
+        while(currentCheck <= checksPerSide) {
+            // Try the offset of the left side
+        	Direction curDir = dir.rotateLeftDegrees(degreeOffset*currentCheck);
+        	loc = rc.getLocation().add(curDir, rc.getType().strideRadius);
+        	
+            if(!rc.hasMoved() && rc.canMove(curDir)) {
+            	
+            	isSafe = true;
+            	
+            	for(BulletInfo b : bullets)
+            	{
+            		if(willCollideWith(b, loc))
+            		{
+            			isSafe = false;
+            			break;
+            		}
+            	}
+            	
+                if(isSafe)
+                {
+                	rc.move(curDir);
+                	return true;
+                }
+            }
+            // Try the offset on the right side
+        	curDir = dir.rotateRightDegrees(degreeOffset*currentCheck);
+        	loc = rc.getLocation().add(curDir, rc.getType().strideRadius);
+        	
+            if(!rc.hasMoved() && rc.canMove(curDir)) {
+            	
+            	isSafe = true;
+            	
+            	for(BulletInfo b : bullets)
+            	{
+            		if(willCollideWith(b, loc))
+            		{
+            			isSafe = false;
+            			break;
+            		}
+            	}
+            	
+                if(isSafe)
+                {
+                	rc.move(curDir);
+                	return true;
+                }
+            }
+            // No move performed, try slightly further
+            currentCheck++;
+        }
+
+        // A move never happened, so return false.
+       // Micro.dodge();
+        return false;
+    }
+    
+    
+    public static void wander() throws GameActionException {
+        Direction dir = randomDirection();
+        tryMove(dir);
+    }
+    
+    // Placeholder for now, should have pathfinding later
+    public static void moveTo(MapLocation location) throws GameActionException {
+    	tryMove(rc.getLocation().directionTo(location));
+    }
+}
